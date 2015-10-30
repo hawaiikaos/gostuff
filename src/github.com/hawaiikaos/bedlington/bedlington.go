a82138cc7c5213360cd206ae84a89357296c6d06
Katharine Osborne <katharine.osborne@possible.com>
2015-10-30 18:27:55 +0000

    WIP for adding topics and voting

--- src/github.com/hawaiikaos/bedlington/bedlington.go
+++ src/github.com/hawaiikaos/bedlington/bedlington.go
@@ -3,11 +3,12 @@ package bedlington
 import (
     "net/http"
     "html/template"
+    "fmt"
     //"time"
 
-    //"appengine"
+    "appengine"
     //"appengine/user"
-    //"appengine/datastore"
+    "appengine/datastore"
 )
 
 type Page struct {
@@ -15,8 +16,20 @@ type Page struct {
     //Body []byte
 }
 
+type Topic struct {
+    TopicName string
+}
+
+type Vote struct {
+    Topic Topic
+    Cast string
+}
+
 var templatepath = "templates/"
-var templates = template.Must(template.ParseFiles(templatepath+"root.html"))
+var templates = template.Must(template.ParseFiles(  templatepath+"root.html",
+                                                    templatepath+"vote.html",
+                                                    templatepath+"addtopic.html",
+                                                    templatepath+"tally.html"))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
     err := templates.ExecuteTemplate(w, tmpl+".html", p)
@@ -27,9 +40,62 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 
 func init() {
     http.HandleFunc("/", root)
+    http.HandleFunc("/vote/", vote)
+    http.HandleFunc("/vote/addtopic/", addtopic)
+    http.HandleFunc("/vote/tally/", tally)
+    http.HandleFunc("/test/", test)
+}
+
+func vote(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("in vote function")
+    var p Page
+    p.Title = "Vote"
+    renderTemplate(w, "vote", &p)
+}
+
+func test(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("in test")
+    fmt.Fprintf(w, "test")
+    
+}
+
+func topicKey(c appengine.Context) *datastore.Key {
+    return datastore.NewKey(c, "Topics", "", 0, nil)
+}
+
+func voteKey(c appengine.Context) *datastore.Key {
+    return datastore.NewKey(c, "Votes", "", 0, nil)
+}
+
+func addtopic(w http.ResponseWriter, r *http.Request) {
+    //fmt.Println("in add topic")
+    var p Page
+    p.Title = "Add Topic"
+    
+    c := appengine.NewContext(r)
+    q := datastore.NewQuery("Topic").Ancestor(topicKey(c)).Order("-Date").Limit(20)
+    topics := make([]Topic, 0, 20)
+    if _, err := q.GetAll(c, &topics); err != nil {
+        fmt.Println("in error")
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    } else {
+        renderTemplate(w, "addtopic", &p)
+    }
+    /*if err := renderTemplate(w, "addtopic", &p) {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }*/
+}
+
+func tally(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("in tally")
+    var p Page
+    p.Title = "Tally"
+    renderTemplate(w, "tally", &p)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("in root function")
     //c := appengine.NewContext(r)
     /*if err := geozoomTemplate.Execute(w, "stuff"); err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -40,14 +106,3 @@ func root(w http.ResponseWriter, r *http.Request) {
     
     renderTemplate(w, "root", &p)
 }
-
-var geozoomTemplate = template.Must(template.New("location").Parse(`
-    <html>
-    <head>
-    <title>Bedlington Geozoom</title>
-    </head>
-    <body>
-    You are here
-    </body>
-    </html>
-`))
